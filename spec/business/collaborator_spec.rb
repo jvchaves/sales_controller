79fb83ac5516f9ed8d8a4f091e163db8de90bf26
João Victor Chaves <joao.victor.chaves159@gmail.com>
2023-02-28 15:05:40 -0300

    Test(Collaborator):create tests to validate email

---
+++ spec/business/collaborator_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe 'Email Validate' do
+  it 'accept valid email' do
+    collaborator = Collaborator.create(email: 'email@example.com')
+
+    expect(collaborator.email).to match(/\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i)
+  end
+
+  it 'deve rejeitar um email inválido' do
+    collaborator = Collaborator.create(email: 'email@example')
+    expect(collaborator.email).not_to match(/\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i)
+  end
+end
