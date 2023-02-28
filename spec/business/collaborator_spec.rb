fde06b076a5b1d1d007c4550612bbb1f3a83bd05
João Victor Chaves <joao.victor.chaves159@gmail.com>
2023-02-28 15:07:04 -0300

    Style(Collaborator):refactor test name

--- spec/business/collaborator_spec.rb
+++ spec/business/collaborator_spec.rb
@@ -7,8 +7,9 @@ describe 'Email Validate' do
     expect(collaborator.email).to match(/\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i)
   end
 
-  it 'deve rejeitar um email inválido' do
+  it 'Not accept invalid Email' do
     collaborator = Collaborator.create(email: 'email@example')
+
     expect(collaborator.email).not_to match(/\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i)
   end
 end
